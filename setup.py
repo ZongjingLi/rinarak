787f72a339ee381dddd394a5f8d4ec2280f9f2d4
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2024-01-24 17:48:33 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     packages=find_packages(),
     include_package_data = True,
     package_data={
-        'rinarak': ['*.txt']
+        'rinarak': ['rinarak/*.grammar']
         },
         
     
